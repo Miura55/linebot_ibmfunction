a0f6978134ede990752eb9e769f734786d0b8e2b
Miura55 <acordion.piano@gmail.com>
2020-04-11 21:06:09 +0900

    Returned default

--- actions/echobot.py
+++ actions/echobot.py
@@ -1,8 +1,6 @@
 # coding: utf-8
-
-import json
 from linebot import (
-    WebhookHandler, LineBotApi
+    LineBotApi
 )
 from linebot.exceptions import (
     InvalidSignatureError,
@@ -10,8 +8,6 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     TextSendMessage,
-    MessageEvent,
-    TextMessage,
 )
 
 
@@ -19,13 +15,6 @@ def main(args):
     print(args)
     # account setting
     line_bot_api = LineBotApi(args['CHANNEL_ACCESS_TOKEN'])
-    handler = WebhookHandler(args["CHANNEL_SECRET"])
-    signature = args["__ow_headers"]["x-line-signature"]
-    @handler.add(MessageEvent, message=TextMessage)
-    def handle_message(event):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text))
 
     # make responce
     body = args["events"][0]
@@ -33,8 +22,9 @@ def main(args):
         if body["source"]["userId"] == "Udeadbeefdeadbeefdeadbeefdeadbeef":
             return {"status": 200}
         else:
-            body = json.dumps(args)
-            handler.handle(body, signature)
+            line_bot_api.reply_message(
+                body["replyToken"],
+                TextSendMessage(text=body["message"]["text"]))
     except LineBotApiError as e:
         print("Got exception from LINE Messaging API: %s\n" % e.message)
         for m in e.error.details:
